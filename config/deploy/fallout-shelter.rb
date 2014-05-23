49b3ac55d55e012ed017deba1c2522b00656dbd3
Kornelius Kalnbach <murphy@rubychan.de>
2014-05-23 21:37:21 +0200

    remove deploy details

--- config/deploy/fallout-shelter.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :production
-
-server 'fallout-shelter', user: `whoami`.chomp, roles: %w(app web db), :primary => true
-
-set :deploy_to, '/var/www/Kornelius-Kalnbach/firsthaibane.rubychan.de'
